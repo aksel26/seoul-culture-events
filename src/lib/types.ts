27c6251f6f01bf70af90152d0f20c746434e6ff1
aksel26 <kevinxkim2023@gmail.com>
2025-05-04 18:36:40 +0900

    type

---
+++ src/lib/types.ts
@@ -0,0 +1,35 @@
+export interface TCulturalEventInfo {
+  list_total_count: number;
+  RESULT: TResultCode;
+  TList: TList[];
+}
+
+export interface TResultCode {
+  CODE: string;
+  MESSAGE: string;
+}
+
+export interface TList {
+  CODENAME: string;
+  GUNAME: string;
+  TITLE: string;
+  DATE: string;
+  PLACE: string;
+  ORG_NAME: string;
+  USE_TRGT: string;
+  USE_FEE: string;
+  PLAYER: string;
+  PROGRAM: string;
+  ETC_DESC: string;
+  ORG_LINK: string;
+  MAIN_IMG: string;
+  RGSTDATE: string;
+  TICKET: string;
+  STRTDATE: string;
+  END_DATE: string;
+  THEMECODE: string;
+  LOT: string;
+  LAT: string;
+  IS_FREE: string;
+  HMPG_ADDR: string;
+}
