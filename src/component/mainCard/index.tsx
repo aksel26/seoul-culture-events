fec67297c1ab53cc1d8cb9a9c0e0d805b4eadfb9
aksel26 <kevinxkim2023@gmail.com>
2025-05-05 02:01:24 +0900

    Card 컴포넌트 fetching

--- src/component/mainCard/index.tsx
+++ src/component/mainCard/index.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import { TList } from "../../lib/types";
+import { IconMapPin } from "@tabler/icons-react";
+// import PlaceTag from "../tag";
 
 const ContentCard = ({ item }: { item: TList }) => {
-  console.log("item: ", item);
+  const redirectPage = (url: string) => {
+    window.open(url, "_blank");
+  };
   return (
-    <div className="flex flex-col w-full">
-      <img
-        src="https://culture.seoul.go.kr/cmmn/file/getImage.do?atchFileId=42afe00583eb4b0983dba37a04a41222&thumb=Y"
-        className="w-full md:max-w-50"
-      />
-      <div className="mt-2">
-        <p>{item.TITLE}</p>
-        <p className="text-xs">{item.DATE}</p>
-        <div className="flex justify-between">
+    <div
+      className="flex flex-col w-full relative overflow-hidden"
+      onClick={() => redirectPage(item.HMPG_ADDR)}
+    >
+      <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent rounded-md"></div>
+
+      <img src={item.MAIN_IMG} className="w-full md:max-w-50 rounded-md" />
+
+      <div className="absolute bottom-0 left-0 right-0 p-4 text-white">
+        <p className="font-bold">{item.TITLE}</p>
+        <p className="text-sm text-slate-50">{item.DATE}</p>
+        <div className="flex justify-between items-center">
           <p className="text-xs">{item.CODENAME}</p>
-          <p className="text-xs">{item.GUNAME}</p>
+          <div className="flex gap-x-1 items-center">
+            <IconMapPin size={20} strokeWidth={1.2} />
+            <p className="text-xs">{item.GUNAME}</p>
+          </div>
+          {/* <PlaceTag>{item.GUNAME}</PlaceTag> */}
         </div>
       </div>
     </div>
