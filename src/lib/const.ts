7e7cccc3fadea97cdbe864e6cf503b6c95d23ab2
aksel26 <kevinxkim2023@gmail.com>
2025-05-06 17:20:41 +0900

    const 값 추가

---
+++ src/lib/const.ts
@@ -0,0 +1,51 @@
+import { createMonthArray } from "@/utils/createMonth";
+import { MonthOption } from "./types";
+
+export const CATEGORY = [
+  { value: "문화교양/강좌", label: "문화교양/강좌" },
+  { value: "전시/미술", label: "전시/미술" },
+  { value: "뮤지컬/오페라", label: "뮤지컬/오페라" },
+  { value: "기타", label: "기타" },
+  { value: "연극", label: "연극" },
+  { value: "무용", label: "무용" },
+  { value: "영화", label: "영화" },
+  { value: "국악", label: "국악" },
+  { value: "콘서트", label: "콘서트" },
+  { value: "클래식", label: "클래식" },
+  { value: "독주/독창회", label: "독주/독창회" },
+  { value: "축제-문화/예술", label: "축제-문화/예술" },
+  { value: "축제-전통/역사", label: "축제-전통/역사" },
+  { value: "축제-시민화합", label: "축제-시민화합" },
+  { value: "축제-기타", label: "축제-기타" },
+  { value: "축제-자연/경관", label: "축제-자연/경관" },
+];
+
+// export const CITY = [
+//   { value: "강남구", label: "강남구" },
+//   { value: "강동구", label: "강동구" },
+//   { value: "강서구", label: "강서구" },
+//   { value: "관악구", label: "관악구" },
+//   { value: "광진구", label: "광진구" },
+//   { value: "구로구", label: "구로구" },
+//   { value: "금천구", label: "금천구" },
+//   { value: "노원구", label: "노원구" },
+//   { value: "도봉구", label: "도봉구" },
+//   { value: "동작구", label: "동작구" },
+//   { value: "마포구", label: "마포구" },
+//   { value: "북구", label: "북구" },
+//   { value: "서대문구", label: "서대문구" },
+//   { value: "서초구", label: "서초구" },
+//   { value: "성동구", label: "성동구" },
+//   { value: "성북구", label: "성북구" },
+//   { value: "성북구", label: "성북구" },
+//   { value: "송파구", label: "송파구" },
+//   { value: "양천구", label: "양천구" },
+//   { value: "영등포구", label: "영등포구" },
+//   { value: "용산구", label: "용산구" },
+//   { value: "은평구", label: "은평구" },
+//   { value: "종로구", label: "종로구" },
+//   { value: "중구", label: "중구" },
+//   { value: "중랑구", label: "중랑구" },
+// ];
+
+export const MONTH: MonthOption[] = createMonthArray();
