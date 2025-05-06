1830b2c8b7608673e0fbcd59e2afaba24b03091b
aksel26 <kevinxkim2023@gmail.com>
2025-05-06 17:21:08 +0900

    store fetch test

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import { TCulturalEventInfo } from "../lib/types";
 import apiClient from "../services/api";
+import useCategoryStore from "../store/useStore";
 
 const useFetch = () => {
   const KEY = import.meta.env.VITE_OPEN_API_KEY;
+
+  const { params } = useCategoryStore();
+  console.log("params: ", params);
 
   const [result, setResult] = useState<TCulturalEventInfo>();
 
@@ -12,7 +16,7 @@ const useFetch = () => {
     const fetchUsers = async () => {
       setIsLoading(true);
       const fetchResult = await apiClient.get(
-        `http://openapi.seoul.go.kr:8088/${KEY}/json/culturalEventInfo/1/10`
+        `http://openapi.seoul.go.kr:8088/${KEY}/json/culturalEventInfo/1/10/축제-시민화합`
       );
 
       setIsLoading(false);
