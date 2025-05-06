ddb0b4309209df4e8dfa352a30effb3a27c678fe
aksel26 <kevinxkim2023@gmail.com>
2025-05-06 17:19:44 +0900

    디렉토리 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Header from "./component/header";
-import Content from "./component/content";
-import Footer from "./component/footer";
+import Header from "./components/header";
+import Content from "./components/content";
+import Footer from "./components/footer";
 import "./App.css";
-import ToTopButton from "./component/topButton";
+import ToTopButton from "./components/topButton";
 function App() {
   return (
-    <div className="w-screen bg-gradient-to-b from-mint-50 to-pink-100 break-keep">
+    <div className="w-screen bg-gradient-to-b from-primary-50 to-pink-100 break-keep">
       <div className="max-w-2xl mx-auto">
         <header>
           <Header />
