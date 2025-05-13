4755e31a72fdeb82b8aa274dc69e3756aa61c645
aksel26 <kevinxkim2023@gmail.com>
2025-05-13 11:11:38 +0900

    구조변경

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,23 @@ import Events from "./components/events";
 import PlaceTag from "./component/ui/tag";
 import { CATEGORY } from "./lib/const";
 import Title from "./components/title";
-import hatchImg from "../src/assets/header.png";
+
+import useFetch from "./hooks/useFetch";
+import useGeolocation from "./hooks/useGeoLocation";
+import Header from "./components/header";
+import Loader from "./components/loader";
 function App() {
+  const { result, isLoading } = useFetch();
+
+  const { location, error, loading } = useGeolocation();
+  console.log("🚀 ~ App ~ location, error, loading:", location, error, loading);
+
+  if (isLoading) return <Loader />;
   return (
     <div className="w-screen h-screen break-keep ">
-      <header className="sticky top-0  bg-white shadow-xs transition-opacity duration-300 z-50 ">
-        <div className="container mx-auto px-4 relative py-3">
-          <div className="text-center text-base font-bold text-primary">서울시 문화공연</div>
-          <img src={hatchImg} alt="header" className="h-10 absolute bottom-0 left-5" />
-        </div>
-      </header>
+      <Header />
       <main className="max-w-2xl mx-auto">
-        <Title />
+        <Title result={result} />
 
         <section className="z-10 relative bg-gradient-to-b from-primary-50 to-pink-100 pt-10">
           <div className="absolute -top-4 left-0 right-0 h-12 bg-gradient-to-b from-white via-primary-50 to-primary-50"></div>
@@ -26,7 +31,7 @@ function App() {
               <PlaceTag key={item.value}>{item.label}</PlaceTag>
             ))}
           </div>
-          <Events />
+          <Events result={result} isLoading={isLoading} />
         </section>
 
         <ToTopButton />
