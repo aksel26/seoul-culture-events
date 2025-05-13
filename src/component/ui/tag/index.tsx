5f2e70b9685ae99e38b181a5eee45483ecc7a4e9
aksel26 <kevinxkim2023@gmail.com>
2025-05-13 17:02:44 +0900

    tag css

--- src/component/ui/tag/index.tsx
+++ src/component/ui/tag/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const PlaceTag = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="bg-primary-700 rounded-md p-1 px-2">
-      <p className="text-base text-primary-50">{children}</p>
+      <p className="text-sm text-primary-50">{children}</p>
     </div>
   );
 };
