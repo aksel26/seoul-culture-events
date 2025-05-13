c0f7df6f2da89af1020e5ee3c7a4c3391490af85
aksel26 <kevinxkim2023@gmail.com>
2025-05-13 17:02:36 +0900

    events css

--- src/components/events/index.tsx
+++ src/components/events/index.tsx
@@ -8,21 +8,14 @@ const Events = ({ result, isLoading }: { result: TCulturalEventInfo | undefined;
 
   if (isLoading) return <Loader />;
   return (
-    <div className="p-6 z-20 h-full snap-center">
-      <div className="flex justify-between mb-2">
-        <p className="text-lg">최근 10개 올라온 문화행사 </p>
-        <button className="text-base text-slate-400">더보기 </button>
-      </div>
-
-      <div className="h-[600px] overflow-y-auto snap-y snap-mandatory scroll-p-4">
-        {result?.row.map((item: TList, index: number) => (
-          <React.Fragment key={index}>
-            <div className="p-1 snap-center">
-              <ContentCard item={item} />
-            </div>
-          </React.Fragment>
-        ))}
-      </div>
+    <div className="p-6 z-20 h-full">
+      {result?.row.map((item: TList, index: number) => (
+        <React.Fragment key={index}>
+          <div className="p-1 snap-center">
+            <ContentCard item={item} />
+          </div>
+        </React.Fragment>
+      ))}
     </div>
   );
 };
