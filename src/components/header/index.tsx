96b10e9c9859a1fdcdf3a3c6ce09847021e6f51e
aksel26 <kevinxkim2023@gmail.com>
2025-05-13 11:09:53 +0900

    Header컴포넌트

---
+++ src/components/header/index.tsx
@@ -0,0 +1,14 @@
+import hatchImg from "../../assets/header.png";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0  bg-white shadow-xs transition-opacity duration-300 z-50 ">
+      <div className="container mx-auto px-4 relative py-3">
+        <div className="text-center text-base font-bold text-primary">서울시 문화공연</div>
+        <img src={hatchImg} alt="header" className="h-10 absolute bottom-0 left-5" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
