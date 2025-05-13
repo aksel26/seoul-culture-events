e0a96ff0fa52984cfa60243df0c53d8e4f737bdd
aksel26 <kevinxkim2023@gmail.com>
2025-05-13 17:00:54 +0900

    react-router설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,19 @@
-import Footer from "./components/footer";
 import "./App.css";
 
-import ToTopButton from "./components/topButton";
-import Events from "./components/events";
-import PlaceTag from "./component/ui/tag";
-import { CATEGORY } from "./lib/const";
-import Title from "./components/title";
-
-import useFetch from "./hooks/useFetch";
-import useGeolocation from "./hooks/useGeoLocation";
-import Header from "./components/header";
-import Loader from "./components/loader";
+import { Route, Routes } from "react-router-dom";
+import NotFound from "./pages/404";
+import Discover from "./pages/discover";
+import Home from "./pages/home";
+import Layout from "./components/layout";
 function App() {
-  const { result, isLoading } = useFetch();
-
-  const { location, error, loading } = useGeolocation();
-  console.log("🚀 ~ App ~ location, error, loading:", location, error, loading);
-
-  if (isLoading) return <Loader />;
   return (
-    <div className="w-screen h-screen break-keep ">
-      <Header />
-      <main className="max-w-2xl mx-auto">
-        <Title result={result} />
-
-        <section className="z-10 relative bg-gradient-to-b from-primary-50 to-pink-100 pt-10">
-          <div className="absolute -top-4 left-0 right-0 h-12 bg-gradient-to-b from-white via-primary-50 to-primary-50"></div>
-          <div className="flex flex-wrap gap-2 mb-4 px-6">
-            {CATEGORY.map((item) => (
-              <PlaceTag key={item.value}>{item.label}</PlaceTag>
-            ))}
-          </div>
-          <Events result={result} isLoading={isLoading} />
-        </section>
-
-        <ToTopButton />
-        <Footer />
-      </main>
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/discover" element={<Discover />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
