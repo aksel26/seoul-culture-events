d18a20a62cb9f061ceda5ef19e7a83050d500bc0
aksel26 <kevinxkim2023@gmail.com>
2025-05-13 17:02:10 +0900

    header문구 변경

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="sticky top-0  bg-white shadow-xs transition-opacity duration-300 z-50 ">
       <div className="container mx-auto px-4 relative py-3">
-        <div className="text-center text-base font-bold text-primary">서울시 문화공연</div>
+        <div className="text-center text-sm font-bold text-primary">서울시 문화공연 검색 서비스</div>
         <img src={hatchImg} alt="header" className="h-10 absolute bottom-0 left-5" />
       </div>
     </header>
