49402fce6050f9c5f78af87e109e7c7494aca4bc
aksel26 <kevinxkim2023@gmail.com>
2025-05-24 18:00:21 +0900

    header 로고 적용

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,17 @@
 import hatchImg from "../../assets/header.png";
+import headerLogo from "../../assets/headerLogo.png";
 
 const Header = () => {
   return (
     <header className="max-w-2xl mx-auto sticky top-0  bg-white shadow-xs transition-opacity duration-300 z-50 ">
-      <div className="container mx-auto px-4 relative py-3">
-        <div className="text-center text-sm font-bold text-primary">서울시 문화공연 검색 서비스</div>
-        <img src={hatchImg} alt="header" className="h-10 absolute bottom-0 left-5" />
+      <div className="container mx-auto px-1 relative py-1 flex justify-center">
+        <img src={headerLogo} alt="headerLogo" className="h-10" />
+
+        <img
+          src={hatchImg}
+          alt="header"
+          className="h-10 absolute bottom-0 left-5"
+        />
       </div>
     </header>
   );
